c38289c737b8e3ce2e1d19a3d38f46e7450b49b2
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-13 15:29:57 +0100

    Def

--- script.js
+++ script.js
@@ -7,22 +7,22 @@
 */
 
 
-let kmPercorsi = parseInt(prompt("Quanti km devi percorrere ?"))
-let eta = parseInt(prompt("Quanti anni hai ?")) 
+let kmPercorsi = parseInt(prompt("Quanti km devi percorrere ?"));
+let eta = parseInt(prompt("Quanti anni hai ?"));
 
-let costoBiglietto = kmPercorsi * 0.21
+let costoBiglietto = kmPercorsi * 0.21;
 let prezzoScontato;
 
 if (eta < 18 ){
     let discount = 20;
-    prezzoScontato = costoBiglietto * (1- discount/100)
+    prezzoScontato = costoBiglietto * (1- discount/100);
 
 } else if (eta >= 65) {
     let discount = 40;
-    prezzoScontato = costoBiglietto * (1- discount/100)
+    prezzoScontato = costoBiglietto * (1- discount/100);
 }
 else{
-    prezzoScontato = costoBiglietto
+    prezzoScontato = costoBiglietto;
 }
 
 console.log(`Il costo totale è di ${prezzoScontato.toFixed(2)} €`);
